b96c615748efdf489c3ff68ad5da5028a9cd507b
zmzaxg <68527322+zmzaxg@users.noreply.github.com>
2026-03-15 07:48:48 +0800

    Update [[route]].js

--- functions/twikoo/[[route]].js
+++ functions/twikoo/[[route]].js
@@ -13,7 +13,12 @@ export async function onRequest(context) {
       allowLogin: false,
       allowRegister: false,
       // 👇 开启邮件通知
-      mail: true
+      mail: false, // 关闭邮件，避免报错
+       // 👇 新增 pushoo 推送
+       pushoo: {
+         channel: 'wecom', // 企业微信
+         token: env.TWIKOO_WECOM_TOKEN // 从环境变量读取
+       }
     },
     // 👇 把你的邮箱设为管理员（收通知）
     master: ['zmzaxg@qq.com'],
